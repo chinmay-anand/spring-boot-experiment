8130519c46b717593478d33c23a7c28ee7582aca
chinmay-anand <chinmay.subscriptions@gmail.com>
2021-03-11 19:07:22 +0530

    Code for getting specific topic is added.

--- course-api/src/main/java/com/chinmay/springbootexperiment/topic/TopicService.java
+++ course-api/src/main/java/com/chinmay/springbootexperiment/topic/TopicService.java
@@ -2,6 +2,7 @@ package com.chinmay.springbootexperiment.topic;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.stereotype.Service;
 
@@ -16,5 +17,13 @@ public class TopicService {
 
 	public List<Topic> getAllTopics(){
 		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		//Traverses thru the list of topics to get the first topic with matching id and returns it
+		//If there is no match then java.util.NoSuchElementException is thrown and browser show Whitelabel Error  
+		return topics.stream()
+				.filter(tpc -> tpc.getId().equals(id))
+				.findFirst().get();
 	}
 }
