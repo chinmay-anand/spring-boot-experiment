536fa52417aed7a9b9bfe35c5b119103347b5c64
chinmay-anand <chinmay.subscriptions@gmail.com>
2021-03-11 16:43:29 +0530

    added comment to controller for next planned action.

--- course-api/src/main/java/com/chinmay/springbootexperiment/topics/TopicController.java
+++ course-api/src/main/java/com/chinmay/springbootexperiment/topics/TopicController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TopicController {
 	@RequestMapping("/topics")
 	public String getAllTopics() {
+		//TODO: Next action is to replace this function to return a list of Topic java objects as json. 
 		return "All Topics";
 	}
 }
